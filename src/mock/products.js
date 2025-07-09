73d4ec5160dec9088359b266ff141792ce8e3d37
Mairely Rosas <116728945+Mairely19@users.noreply.github.com>
2025-07-08 21:15:35 -0400

    Add files via upload

---
+++ src/mock/products.js
@@ -0,0 +1,14 @@
+export const initialProducts = [
+  {
+    id: 1,
+    name: "Laptop HP",
+    price: 1200,
+    stock: 15
+  },
+  {
+    id: 2,
+    name: "Mouse Inalámbrico",
+    price: 25,
+    stock: 8
+  }
+];
